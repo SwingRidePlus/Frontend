ead2a1d90e8900b14d3b1b13ea2361778bbde1bb
Jamkris <dltmdgus1412@gmail.com>
2024-10-29 21:56:59 +0900

    FIX :: env 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { useRoutes } from 'react-router-dom';
 
 import Main from 'pages/Main';
+import SearchLocation from 'pages/SearchLocation';
 
 export default function Router() {
   return useRoutes([
     {
       path: '/',
       children: [{ index: true, element: <Main /> }]
+    },
+    {
+      path: '/searchlocation',
+      children: [{ index: true, element: <SearchLocation /> }]
     }
     // { path: '*', element: <NotFound /> }
   ]);
