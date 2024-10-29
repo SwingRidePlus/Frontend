d4b0a43576ee9eb4049454fedcb6e07102cb3613
je355 <chaeeehui@gmail.com>
2024-10-30 03:00:36 +0900

    feat :: 캘린더

---
+++ src/pages/MyReservation/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react';
+import * as _ from './style';
+import MenuBar from 'components/MenuBar';
+import Calendar from 'components/Calendar';
+import UnReservation from 'components/ReservationBox/UnReservation/index';
+
+const MyReservation = () => {
+  const [selectedDays, setSelectedDays] = useState<{ start: Date | null; end: Date | null }>({ start: null, end: null });
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const handlePrevMonth = useCallback(() => {
+    setCurrentMonth(prev => new Date(prev.getFullYear(), prev.getMonth() - 1, 1));
+  }, []);
+
+  const handleNextMonth = useCallback(() => {
+    setCurrentMonth(prev => new Date(prev.getFullYear(), prev.getMonth() + 1, 1));
+  }, []);
+
+  return (
+    <_.Main_Container>
+      <div>
+        <_.MainTitle>내 예약</_.MainTitle>
+        <_.CalendarContainer>
+          <_.MonthNavigation>
+            <_.MonthButtonText onClick={handlePrevMonth}>이전 달</_.MonthButtonText>
+            <span>{currentMonth.getFullYear()}년 {currentMonth.getMonth() + 1}월</span>
+            <_.MonthButtonText onClick={handleNextMonth}>다음 달</_.MonthButtonText>
+          </_.MonthNavigation>
+          <Calendar
+            selectedDays={selectedDays}
+            setSelectedDays={setSelectedDays}
+            currentMonth={currentMonth}
+          />
+        </_.CalendarContainer>
+      </div>
+      
+      <_.UnReservationContainer>
+        <UnReservation selectedDays={selectedDays} />
+      </_.UnReservationContainer>
+
+      <MenuBar selectState={2} />
+    </_.Main_Container>
+  );
+};
+
+export default MyReservation;
---
+++ src/pages/MyReservation/style.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Main_Container = styled.div`
+  width: 100vw;
+  min-height: 100vh; 
+  padding: 42px 32px 52px;
+  display: flex;
+  flex-direction: column; 
+  justify-content: flex-start; 
+  position: relative; 
+`;
+
+export const MainTitle = styled.div`
+    color: var(--Black, #000);
+
+    /* Title1 */
+    font-family: Pretendard;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%; /* 36.4px */
+`
+export const MonthNavigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  button {
+    background-color: #FF6701;
+    border: none;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border-radius: 30px;
+
+    &:hover {
+      background-color: #FF5C00;
+    }
+  }
+
+  span {
+    font-weight: bold;
+  }
+`;
+
+export const MonthButtonText = styled.button`
+  color: white;
+`
+
+export const CalendarContainer = styled.div`
+  padding: 40px 0 0 0;
+`
+
+export const UnReservationContainer = styled.div`
+  padding: 20px 0 0 0;
+`
